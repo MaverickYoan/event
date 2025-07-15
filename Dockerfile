44c487313f6a153b4688deb4093d627796487a81
MaverickYoan <ensemblepourlafetelibre@gmail.com>
2025-07-15 03:30:07 +0200

    conn_pgadmin&php

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM php:8.3-apache
 
 RUN apt-get update && apt-get install -y libpq-dev && rm -rf /var/lib/apt/lists/*
-RUN docker-php-ext-install pdo pdo_pgsql && docker-php-ext-enable pdo pdo_pgsql
+RUN docker-php-ext-install pdo pdo_pgsql && docker-php-ext-enable pdo pdo_pgsql 
 
 EXPOSE 80
 
---
+++ src/index.php
@@ -0,0 +1,20 @@
+<?php
+$servername = "localhost";
+$username = "admin";
+$password = "admin";
+$dbname = "project_event";
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
